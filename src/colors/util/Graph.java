cf44df6a4ca4e9dc9dacd3305e7dcfeb4781ee42
Josh Hansen <hansen.joshuaa@gmail.com>
2011-02-23 20:56:26 +0000

    A graph that considers time

--- src/colors/util/Graph.java
+++ src/colors/util/Graph.java
@@ -12,12 +12,8 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 public class Graph {
-	private static class Spell {
-		
-	}
-	
 	/** This helps us maintain a global list of attributes based on which ones are set at the node and edge level */
-	private static class SetUpdatingMap<K,V> extends HashMap<K,V> {
+	public static class SetUpdatingMap<K,V> extends HashMap<K,V> {
 		private final Set<K> set;
 		
 		public SetUpdatingMap(Set<K> set) {
